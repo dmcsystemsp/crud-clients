318f28e892f5f5e3ed0132cafac97cb50a2cd66a
Daniel Carneiro <daniel@dmcsysteminformatica.com.br>
2022-09-23 15:53:39 -0300

    Delete Client

--- src/main/java/com/dmcsystemsp/crudclientes/services/ClientService.java
+++ src/main/java/com/dmcsystemsp/crudclientes/services/ClientService.java
@@ -3,12 +3,14 @@ package com.dmcsystemsp.crudclientes.services;
 import com.dmcsystemsp.crudclientes.dto.ClientDTO;
 import com.dmcsystemsp.crudclientes.entities.Client;
 import com.dmcsystemsp.crudclientes.repositories.ClientRepository;
+import jakarta.persistence.Column;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +31,40 @@ public class ClientService {
         return result.map(x -> new ClientDTO(x));
 
     }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+
+        copyDtoToEntity(dto, entity);
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        Client entity = repository.getReferenceById(id);
+
+        copyDtoToEntity(dto, entity);
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id){
+       repository.deleteById(id);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+    }
+
 
 }
